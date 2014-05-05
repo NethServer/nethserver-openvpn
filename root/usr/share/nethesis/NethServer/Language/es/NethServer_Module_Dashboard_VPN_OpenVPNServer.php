5a6c289fa4974a0b689de3ecabbec277ca6595d2
Victor Moreno <victor_moreno_rubio@hotmail.com>
2014-05-05 15:27:40 +0200

    Web interface: add Spanish translation. Refs #2732

---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_Dashboard_VPN_OpenVPNServer.php
@@ -0,0 +1,15 @@
+<?php
+
+$L['ovpn_server'] = "Servidor OpenVPN";
+$L['ovpn_disabled'] = 'Servidor OpenVPN deshabilitado';
+$L['enabled'] = 'Activado';
+$L['disabled'] = 'Desactivado';
+$L['routed'] = 'Enrutado';
+$L['bridged'] = 'Puente';
+$L['password'] = 'Contraseña';
+$L['certificate'] = 'Certificado';
+$L['password-certificate'] = 'Contraseña y certificado';
+$L['ovpn_auth'] = 'Autentificación';
+$L['ovpn_mode'] = 'Modo';
+$L['ovpn_range'] = 'Rango IP';
+$L['ovpn_port'] = 'Puerto UDP';
---
+++ root/usr/share/nethesis/NethServer/Language/es/NethServer_Module_VPN_OpenVPN.php
@@ -0,0 +1,22 @@
+<?php
+
+$L['ServerStatus_label'] = 'Activar el servidor roadwarrior';
+$L['OpenVPN_Description'] = 'Configuración de OpenVPN';
+$L['OpenVPN_Tags'] = 'vpn virtual de roadwarrior openvpn red privada';
+$L['OpenVPN_Title'] = 'OpenVPN';
+$L['AuthMode_label'] = 'Modo de autentificación';
+$L['password_mode_label'] = 'Nombre de usuario y contraseña';
+$L['certificate_mode_label'] = 'Certificado';
+$L['password_certificate_mode_label'] = 'Nombre de usuario, contraseña y certificado';
+$L['Network_label'] = 'Red';
+$L['Netmask_label'] = 'Máscara';
+$L['ClientToClient_label'] = 'Permitir el tráfico de red cliente a cliente';
+$L['enabled_label'] = 'Si';
+$L['disabled_label'] = 'No';
+$L['Mode_routed_label'] = 'Modo enrutado';
+$L['RouteToVPN_label'] = 'Ruta de todo el tráfico de clientes a través de VPN';
+$L['Mode_bridged_label'] = 'Modo puente';
+$L['BridgeStartIP_label'] = 'Inicio del rango IP';
+$L['BridgeEndIP_label'] = 'Parada del rango IP';
+$L['RoutedAdvanced_label'] = 'Avanzado';
+$L['Compression_label'] = 'Habilitar la compresión LZO';
