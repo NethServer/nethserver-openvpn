e6e4081a921ab16bae4119112a8603a75c097118
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-05-12 15:31:15 +0200

    Translation: remove Italian language

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_Dashboard_VPN_OpenVPNClients.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$L['Key_label'] = 'Nome';
-$L['real_address_label'] = 'Indirizzo reale';
-$L['virtual_address_label'] = 'Indirizzo virtuale';
-$L['bytes_label'] = 'Traffico';
-$L['since_label'] = 'Connesso da';
-$L['Kill_label'] = 'Termina';
