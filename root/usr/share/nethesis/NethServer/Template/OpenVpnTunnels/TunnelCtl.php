70da8d64ffcade93d340fcd91208a4679d016f30
Davide Principi <davide.principi@nethesis.it>
2017-06-29 15:24:35 +0200

    Show tunnel name in enable/disable action dialog

--- root/usr/share/nethesis/NethServer/Template/OpenVpnTunnels/TunnelCtl.php
+++ root/usr/share/nethesis/NethServer/Template/OpenVpnTunnels/TunnelCtl.php
@@ -1,10 +1,11 @@
 <?php
 /* @var $view \Nethgui\Renderer\Xhtml */
-echo $view->header()->setAttribute('template', $T('OpenVpnTunnels_'.$view->getModule()->getIdentifier().'_title'));
+echo $view->header('service')->setAttribute('template', $T('OpenVpnTunnels_'.$view->getModule()->getIdentifier().'_title'));
 
 $view->requireFlag($view::INSET_DIALOG);
 
-echo $view->translate('confirm_'.$view->getModule()->getIdentifier().'_label');
+$message = $view->getModule()->getIdentifier() === 'enable' ? $T('confirm_enable_label') : $T('confirm_disable_label');
+echo $view->textLabel('service')->setAttribute('template', $message);
 
 echo $view->buttonList()
     ->insert($view->button('Confirm', $view::BUTTON_SUBMIT))
