202728ac4aa0ce77680646378068321aa0949632
Davide Principi <davide.principi@nethesis.it>
2017-09-04 11:38:21 +0200

    Include CSRF token in upload form

--- root/usr/share/nethesis/NethServer/Template/OpenVpnTunnels/Clients/Upload.php
+++ root/usr/share/nethesis/NethServer/Template/OpenVpnTunnels/Clients/Upload.php
@@ -1,22 +1,16 @@
 <?php
 
 /* @var $view \Nethgui\Renderer\Xhtml */
-$view->rejectFlag($view::INSET_FORM);
+$view->requireFlag($view::FORM_ENC_MULTIPART);
 
 $actionUrl = $view->getModuleUrl();
 
-echo "<form action=\"{$actionUrl}\" method=\"post\" enctype=\"multipart/form-data\">";
-
 echo $view->header()->setAttribute('template', $T('Upload_Header'));
 
-$idArc = $view->getUniqueId('arc');
-
-echo "<div class=\"labeled-control label-above\"><label for=\"{$idArc}\">" . \htmlspecialchars($T('UploadArc_label')) . "</label><input type=\"file\" name=\"arc\" id=\"{$idArc}\" /></div>";
+echo $view->fileUpload('UploadArc')->setAttribute('htmlName', 'arc');
 
 echo $view->buttonList()
     ->insert($view->button('Upload', $view::BUTTON_SUBMIT))
     ->insert($view->button('Back', $view::BUTTON_CANCEL))
     ->insert($view->button('Help', $view::BUTTON_HELP))
 ;
-
-echo "</form>";
